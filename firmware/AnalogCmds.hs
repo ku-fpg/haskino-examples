4f8c623b8992c0b049e900375d79924fec4dc69c
Mark Grebe <mark.grebe@ku.edu>
2018-02-08 08:37:46 -0600

    Started adding Analog Cmds

---
+++ firmware/AnalogCmds.hs
@@ -0,0 +1,65 @@
+-------------------------------------------------------------------------------
+-- |
+-- Module      :  AnalogCmds
+-- Copyright   :  (c) University of Kansas
+-- License     :  BSD3
+-- Stability   :  experimental
+--
+-- TBD.
+-------------------------------------------------------------------------------
+module AnalogCmds where
+
+import System.Hardware.Haskino
+import Data.Word
+import Data.Bits
+
+import Comms
+import FirmwareCmds 
+
+processAnalogCommand :: [Word8] -> Arduino ()
+processAnalogCommand m = 
+    case head m of
+        c | c == firmwareCmdVal ALG_CMD_READ_PIN   -> processReadPin $ tail m
+          | c == firmwareCmdVal ALG_CMD_WRITE_PIN  -> processWritePin $ tail m
+          | c == firmwareCmdVal ALG_CMD_TONE_PIN   -> processTonePin $ tail m
+          | c == firmwareCmdVal ALG_CMD_NOTONE_PIN -> processNoTonePin $ tail m
+        _                                          -> return ()
+
+processReadPin :: [Word8] -> Arduino ()
+processReadPin m = do
+    if (head m == 0) && (m !! 1 == exprTypeVal EXPR_WORD8)
+    then do
+        a <- analogRead $ m !! 2
+        sendReply (firmwareReplyVal ALG_RESP_READ_PIN) $ ( fromIntegral $ a `shiftR` 8) :  (fromIntegral $ a .&. 8) : []
+    else return ()
+
+processWritePin :: [Word8] -> Arduino ()
+processWritePin m = do
+    if (head m == 0) && (m !! 1 == exprTypeVal EXPR_WORD8) &&
+       (m !! 3 == 0) && (m !! 4 == exprTypeVal EXPR_WORD16)
+    then analogWrite (m !! 2) $ fromIntegral (m !! 5) `shiftL` 8 .|.
+                                fromIntegral (m !! 6)
+    else return ()
+
+processTonePin :: [Word8] -> Arduino ()
+processTonePin m = do 
+    if (head m == 0) && (m !! 1 == exprTypeVal EXPR_WORD8) &&
+       (m !! 3 == 0) && (m !! 4 == exprTypeVal EXPR_WORD16) &&
+       (m !! 7 == 0) && (m !! 8 == exprTypeVal EXPR_WORD32)
+    then do
+        let duration = fromIntegral (m !! 8 ) `shiftL` 24 .|.
+                       fromIntegral (m !! 9 ) `shiftL` 16 .|.
+                       fromIntegral (m !! 10) `shiftL`  8 .|.
+                       fromIntegral (m !! 11)
+            freq =     fromIntegral (m !! 5 ) `shiftL`  8 .|.
+                       fromIntegral (m !! 6)
+        if duration == 0
+        then tone (m !! 2) freq Nothing
+        else tone (m !! 2) freq (Just duration)
+    else return ()
+
+processNoTonePin :: [Word8] -> Arduino ()
+processNoTonePin m = do 
+    if (head m == 0) && (m !! 1 == exprTypeVal EXPR_WORD8) 
+    then noTone (m !! 2) 
+    else return ()
--- firmware/Firmware.hs
+++ firmware/Firmware.hs
@@ -14,6 +14,7 @@ import Data.Word
 import Data.Bits
 
 import FirmwareCmds 
+import AnalogCmds
 import BoardControlCmds
 import BoardStatusCmds
 import Comms
@@ -21,19 +22,23 @@ import DigitalCmds
 
 parseMessage :: [Word8] -> Arduino ()
 parseMessage m = do
-    case (head m) .&. 0xF0 of
-        c | c == firmwareTypeVal BC_CMD_TYPE  -> processBoardControlCommand m
-          | c == firmwareTypeVal BS_CMD_TYPE  -> processBoardStatusCommand m
-          | c == firmwareTypeVal DIG_CMD_TYPE -> processDigitalCommand m
-        {-
-        ALG_CMD_TYPE  -> processAnalogCommand m 
-        I2C_CMD_TYPE  -> processI2CCommand m 
-        SER_CMD_TYPE  -> processSerialCommand m 
-        STEP_CMD_TYPE -> processStepperCommand m 
-        SVRO_CMD_TYPE -> processServoCommand m  
-        REF_CMD_TYPE  -> processRefernceCommand m
-        -}
-        _             -> return ()
+    if null m 
+    then return ()
+    else
+        case (head m) .&. 0xF0 of
+            c | c == firmwareTypeVal BC_CMD_TYPE  -> processBoardControlCommand m
+              | c == firmwareTypeVal BS_CMD_TYPE  -> processBoardStatusCommand m
+              | c == firmwareTypeVal DIG_CMD_TYPE -> processDigitalCommand m
+              | c == firmwareTypeVal ALG_CMD_TYPE -> processAnalogCommand m
+            {-
+            ALG_CMD_TYPE  -> processAnalogCommand m 
+            I2C_CMD_TYPE  -> processI2CCommand m 
+            SER_CMD_TYPE  -> processSerialCommand m 
+            STEP_CMD_TYPE -> processStepperCommand m 
+            SVRO_CMD_TYPE -> processServoCommand m  
+            REF_CMD_TYPE  -> processRefernceCommand m
+            -}
+            _             -> return ()
 
 firmware :: Arduino ()
 firmware = do
