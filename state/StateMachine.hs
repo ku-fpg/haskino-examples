ae8a63b014fa2308ba67da4ad82f3febf9191d76
Mark Grebe <mark.grebe@ku.edu>
2018-02-17 20:51:52 -0600

    More state machine work

--- state/StateMachine.hs
+++ state/StateMachine.hs
@@ -11,6 +11,7 @@
 module Main where
 
 import System.Hardware.Haskino
+import Data.Word
 import LCD
 
 hitachi :: LCDController
@@ -26,33 +27,86 @@ hitachi = Hitachi44780 { lcdRS = 8
                      , dotMode5x10 = False
                      }
 
--- Task which will execute on Arduino, write an 'Rock' to the display, delay a
--- second, write a 'Chalk' to the display, delay a second, write a 'Jayhawk'
--- to the display.
+-- | There are 5 keys on the OSepp shield.
+data Key = KeyNone
+         | KeyRight
+         | KeyLeft
+         | KeyUp
+         | KeyDown
+         | KeySelect
+
+-- fromEnum is currently not working with translation,
+-- so we do not use derviving Enum here.
+
+keyValue :: Key -> Word8
+keyValue KeyNone   = 0
+keyValue KeyRight  = 1
+keyValue KeyLeft   = 2
+keyValue KeyUp     = 3
+keyValue KeyDown   = 4
+keyValue KeySelect = 5
+
+getKey :: Arduino Word8
+getKey = do
+    key <- analogKey
+    waitRelease
+    delayMillis 100
+    return key
+      where
+        analogKey :: Arduino Word8
+        analogKey = do
+            v <- analogRead 0
+            case v of
+              _ | v < 30  -> return (keyValue KeyRight)
+              _ | v < 180 -> return (keyValue KeyUp)
+              _ | v < 360 -> return (keyValue KeyDown)
+              _ | v < 535 -> return (keyValue KeyLeft)
+              _ | v < 760 -> return (keyValue KeySelect)
+              _           -> analogKey
+
+        waitRelease :: Arduino ()
+        waitRelease = do
+            v <- analogRead 0
+            if v < 760 then waitRelease else return ()
 
 theProgram :: Arduino ()
 theProgram = do
-  lcd <- lcdRegister hitachi
-  lcdBacklightOn lcd
-  hello lcd
+    lcd <- lcdRegister hitachi
+    lcdBacklightOn lcd
+    stateMachine lcd
 
-hello :: LCD -> Arduino ()
-hello lcd =
-  loop $ do
-    lcdHome lcd
-    lcdWrite lcd $ litString "Rock   "
-    delayMillis 1500   
-    lcdHome lcd
-    lcdWrite lcd $ litString "Chalk  "
-    delayMillis 1500   
-    lcdHome lcd
-    lcdWrite lcd $ litString "Jayhawk"
-    delayMillis 1500
-    lcdClear lcd
+stateMachine :: LCD -> Arduino ()
+stateMachine lcd =
+    state1 $ keyValue KeyNone
+  where
+    state1 :: Word8 -> Arduino ()
+    state1 _ = do
+       key <- getKey
+       state2 key
 
--- Execute this function to run program with firmware interpreter
-lcdExample :: IO ()
-lcdExample = withArduino True "/dev/cu.usbmodem1421" theProgram
+    state2 :: Word8 -> Arduino ()
+    state2 _ = do
+       key <- getKey
+       case key of
+          1 -> state3 key
+          2 -> state4 key
+          _ -> state2 key
+
+    state3 :: Word8 -> Arduino ()
+    state3 _ = do
+       key <- getKey
+       case key of
+          1 -> state4 key
+          2 -> state2 key
+          _ -> state3 key
+
+    state4 :: Word8 -> Arduino ()
+    state4 _ = do
+       key <- getKey
+       case key of
+          1 -> state2 key
+          2 -> state3 key 
+          _ -> state4 key
 
 main :: IO ()
 main = compileProgram theProgram "stateMachine.ino"
